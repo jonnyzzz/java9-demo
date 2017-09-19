d1460f719bdcc36849b8c0df5f226133e0084cc1
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-09-19 19:17:59 +0200

    add diagnostics

--- src/services/src/org/jetbrains/demo9/services/IntelliService.java
+++ src/services/src/org/jetbrains/demo9/services/IntelliService.java
@@ -4,6 +4,6 @@ import java.util.Set;
 
 public class IntelliService {
   public static void request(Set<String> keys) {
-
+    System.out.println("IntelliService in module: " + IntelliService.class.getModule());
   }
 }
