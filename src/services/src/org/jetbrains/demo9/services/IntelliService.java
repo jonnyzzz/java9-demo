77d2a6ec78c588f35e6703ce1c9adc06e2932e6b
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-09-19 19:25:49 +0200

    deprecated

--- src/services/src/org/jetbrains/demo9/services/IntelliService.java
+++ src/services/src/org/jetbrains/demo9/services/IntelliService.java
@@ -3,6 +3,8 @@ package org.jetbrains.demo9.services;
 import java.util.Set;
 
 public class IntelliService {
+
+  @Deprecated(forRemoval = true)
   public static void request(Set<String> keys) {
     System.out.println("IntelliService in module: " + IntelliService.class.getModule());
   }
