7ce3713ac2d976d0ef8d2648428463f2bbad753f
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-09-20 14:08:17 +0200

    simplify examples

--- src/main/src/org/jetbrains/demo9/Main.java
+++ src/main/src/org/jetbrains/demo9/Main.java
@@ -4,7 +4,6 @@ import org.jetbrains.demo9.services.IntelliService;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
-import java.util.Set;
 
 public class Main {
   public static void main(String[] args) {
@@ -18,6 +17,6 @@ public class Main {
     System.out.println();
     System.out.println("in module: " + Main.class.getModule());
 
-    IntelliService.request(Set.of("a", "b", bean.getVmName())).forEach(System.out::println);
+    IntelliService.request();
   }
 }
--- src/services/src/org/jetbrains/demo9/services/IntelliService.java
+++ src/services/src/org/jetbrains/demo9/services/IntelliService.java
@@ -1,33 +1,9 @@
 package org.jetbrains.demo9.services;
 
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class IntelliService {
 
-  private static final Map<String, String> meaningfulData
-          = Collections.unmodifiableMap(new HashMap<>() {{
-    put("IntelliJ", "IDEA");
-    put("San Francisco", "California");
-    put("Java", "One");
-    put("Java 9", "Jigsaw");
-  }});
-
-  private final Set<String> meaningfulSet = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
-          "a", "b", "c"
-  )));
-
-
-  public static List<String> request(Set<String> keys) {
+  public static void request() {
     System.out.println("IntelliService in module: " + IntelliService.class.getModule());
 
-    return keys.stream().map(meaningfulData::get).flatMap(val -> {
-      if (val == null) {
-        return Stream.empty();
-      } else {
-        return Stream.ofNullable(val);
-      }
-    }).collect(Collectors.toList());
   }
 }
