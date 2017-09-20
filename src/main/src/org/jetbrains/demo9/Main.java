a4774f065a8c7f6212a1df0c5d9f875cc763295e
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-09-20 14:12:03 +0200

    logging

--- src/main/src/org/jetbrains/demo9/Main.java
+++ src/main/src/org/jetbrains/demo9/Main.java
@@ -13,6 +13,6 @@ public class Main {
     System.out.println("from " + bean.getVmName() + " " + bean.getVmVersion());
 
     System.out.println();
-    System.out.println("in module: " + Main.class.getModule());
+    System.out.println("Main.class module: " + Main.class.getModule());
   }
 }
