745098fce6c10c43e7459d81d4265b48f4291b47
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-10-01 06:49:01 -0700

    dumb fix

--- src/services/src/org/jetbrains/demo9/services/IntelliService.java
+++ src/services/src/org/jetbrains/demo9/services/IntelliService.java
@@ -4,6 +4,10 @@ public class IntelliService {
 
   @Deprecated(forRemoval = true, since = "2011")
   public static void killMe() {
-    Thread.currentThread().stop(new Throwable("Bye, Java 1.0"));
+    try {
+      Thread.currentThread().stop(new Throwable("Bye, Java 1.0"));
+    } catch (UnsupportedOperationException e) {
+      //TODO: fix me, it may fail in new JVMs
+    }
   }
 }
