3152e1af200caa3da5ee7cc2babb035058ba689d
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-10-01 15:18:31 +0200

    add final

--- src/services/src/org/jetbrains/demo9/services/Helpers.java
+++ src/services/src/org/jetbrains/demo9/services/Helpers.java
@@ -3,13 +3,13 @@ package org.jetbrains.demo9.services;
 import java.util.*;
 
 public class Helpers {
-  Map<String, String> meaningfulData
+  final Map<String, String> meaningfulData
           = Collections.unmodifiableMap(new HashMap<>() {{
     put("IntelliJ", "IDEA");
     put("San Francisco", "California");
   }});
 
-  Set<String> meaningfulSet
+  final Set<String> meaningfulSet
           = Collections.unmodifiableSet(new HashSet<>(
           Arrays.asList(
                   "a", "b", "c"
